1f8fc2e0063fad12f1f2a4179dedd07e58412026
RajDesai18 <rvdesai1807@gmail.com>
2024-12-24 15:37:33 -0700

    Implement initial file and folder structure

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Inter, Poppins } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter-sans",
+  subsets: ["latin"],
+});
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+
+
+export const metadata: Metadata = {
+  title: "Horizon",
+  description: "Horizon is a personal finance app.",
+  icons: {
+    icon: '/icons/logo.svg',
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${poppins.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
